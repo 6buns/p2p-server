298d449277965130322f3a086b7bfd83c5d0128d
adtjha <adtjha@hotmail.com>
2022-04-07 02:18:42 +0530

    Aaahahahah

--- index.js
+++ index.js
@@ -247,10 +247,19 @@ const verifyAPIKey = async (apiKey) => {
 
 const verifySecretKey = async (secret) => {
     console.log(secret)
-    return await keyStoreRef.where('secretKey', '==', secret).get().then((docs) => {
-        console.log(docs.empty, docs[0]);
-        return docs[0].data();
-    }).catch(console.error)
+    try {
+        const snapshot = await keyStoreRef.where('secretKey', '==', secret).get()
+        if (snapshot.empty) {
+            return new Error('No Data related to Secret present')
+        }
+        snapshot.forEach(doc => {
+            console.log(doc.id, '=>', doc.data());
+        });
+        return snapshot[0].data()
+    } catch (error) {
+        return error
+    }
+
 }
 
 const getRoomFromRedis = (roomId, apiKey) => {
