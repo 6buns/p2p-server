ff1c050a12fda42c42abe74afbfdfbe733fb98cf
adtjha <adtjha@hotmail.com>
2022-04-07 05:35:23 +0530

    what

--- index.js
+++ index.js
@@ -283,7 +283,7 @@ const getRoomFromRedis = (roomId, apiKey) => {
 
 const generateSecret = async (apiKey) => {
     const apiHash = crypto.createHash('md5').update(apiKey).digest('hex')
-    // const secretKey = await jose.generateSecret('HS256')
+    const secretKey = await jose.generateSecret('HS256', { extractable: true })
 
     return secretKey
 
