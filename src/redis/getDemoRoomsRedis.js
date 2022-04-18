c30cb0e9581a7f5b719a82fd12bc1878fd4d37af
adtjha <adtjha@hotmail.com>
2022-04-18 20:05:39 +0530

    ;p

--- src/redis/getDemoRoomsRedis.js
+++ src/redis/getDemoRoomsRedis.js
@@ -8,17 +8,21 @@ exports.getDemoRoomsRedis = (roomId) => {
             if (clients > 2) {
                 resolve({ roomData: await client.sendCommand('LPOP', 'demo_rooms') })
             } else {
-                if (!roomId) roomId = `demo_${randomBytes(5).toString('hex').slice(0, 5)}`
-                createdAt = Date.now();
-                roomKeyHash = createHash('md5').update(`${roomId}`).digest('hex');
-                roomData = {
-                    id: roomId,
-                    apiHash: '',
-                    sessionId: randomBytes(20).toString('hex').slice(0, 20),
-                    createdAt,
-                    validTill: createdAt + 86400000,
-                };
-                await client.sendCommand('RPUSH', 'demo_rooms', JSON.stringify({ ...roomData }))
+                try {
+                    if (!roomId) roomId = `demo_${randomBytes(5).toString('hex').slice(0, 5)}`
+                    createdAt = Date.now();
+                    roomKeyHash = createHash('md5').update(`${roomId}`).digest('hex');
+                    roomData = {
+                        id: roomId,
+                        apiHash: '',
+                        sessionId: randomBytes(20).toString('hex').slice(0, 20),
+                        createdAt,
+                        validTill: createdAt + 86400000,
+                    };
+                    await client.sendCommand('RPUSH', 'demo_rooms', JSON.stringify({ ...roomData }))
+                } catch (error) {
+                    reject(error)
+                }
 
                 resolve({ ...roomData })
             }
