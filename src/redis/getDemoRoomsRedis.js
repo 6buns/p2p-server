6e2f3bc0b9f5168a9b0727c0d33ff84a86b2fd3b
adtjha <adtjha@hotmail.com>
2022-04-18 19:29:27 +0530

    check for roomId

--- src/redis/getDemoRoomsRedis.js
+++ src/redis/getDemoRoomsRedis.js
@@ -1,25 +1,29 @@
 const { createHash } = require('crypto');
 const { client } = require('.');
 
-exports.getDemoRoomsRedis = () => {
+exports.getDemoRoomsRedis = (roomId) => {
     return new Promise(async (resolve, reject) => {
-        const clients = await client.sendCommand('LLEN', 'demo_rooms')
-        if (clients > 2) {
-            return { roomData: await client.sendCommand('LPOP', 'demo_rooms') }
-        } else {
-            roomId = `demo_${randomBytes(5).toString('hex').slice(0, 5)}`
-            createdAt = Date.now();
-            roomKeyHash = createHash('md5').update(`${roomId}`).digest('hex');
-            roomData = {
-                id: roomId,
-                apiHash: '',
-                sessionId: randomBytes(20).toString('hex').slice(0, 20),
-                createdAt,
-                validTill: createdAt + 86400000,
-            };
-            await client.sendCommand('RPUSH', 'demo_rooms', JSON.stringify({ ...roomData }))
+        try {
+            const clients = await client.sendCommand('LLEN', 'demo_rooms')
+            if (clients > 2) {
+                resolve({ roomData: await client.sendCommand('LPOP', 'demo_rooms') })
+            } else {
+                if (!roomId) roomId = `demo_${randomBytes(5).toString('hex').slice(0, 5)}`
+                createdAt = Date.now();
+                roomKeyHash = createHash('md5').update(`${roomId}`).digest('hex');
+                roomData = {
+                    id: roomId,
+                    apiHash: '',
+                    sessionId: randomBytes(20).toString('hex').slice(0, 20),
+                    createdAt,
+                    validTill: createdAt + 86400000,
+                };
+                await client.sendCommand('RPUSH', 'demo_rooms', JSON.stringify({ ...roomData }))
 
-            return { ...roomData }
+                resolve({ ...roomData })
+            }
+        } catch (error) {
+            reject(error)
         }
 
     });
