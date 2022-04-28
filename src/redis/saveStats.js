b51e3f333d8c54217e7aed158decd3112b28df87
adtjha <adtjha@hotmail.com>
2022-04-28 16:41:16 +0530

    hey

--- src/redis/saveStats.js
+++ src/redis/saveStats.js
@@ -5,21 +5,23 @@ exports.saveStats = (roomId, name, stats) => {
     return new Promise(async (resolve, reject) => {
         const roomKeyHash = createHash('md5').update(`${roomId}`).digest('hex');
         let roomData = {};
-        try {
-            const data = await client.get(roomKeyHash)
-            roomData = JSON.parse(data);
-            if (roomData.hasOwnProperty(stats)) {
-                roomData[stats][name] = {};
-                roomData[stats][name] = stats;
-            } else {
-                roomData[stats] = {}
-                roomData[stats][name] = {};
-                roomData[stats][name] = stats;
+        if (!roomId.includes('demo')) {
+            try {
+                const data = await client.get(roomKeyHash)
+                roomData = JSON.parse(data);
+                if (roomData.hasOwnProperty(stats)) {
+                    roomData[stats][name] = {};
+                    roomData[stats][name] = stats;
+                } else {
+                    roomData[stats] = {}
+                    roomData[stats][name] = {};
+                    roomData[stats][name] = stats;
+                }
+                await client.set(roomKeyHash, { ...roomData })
+                resolve();
+            } catch (error) {
+                reject(error)
             }
-            await client.set(roomKeyHash, { ...roomData })
-            resolve();
-        } catch (error) {
-            reject(error)
         }
     });
 }
