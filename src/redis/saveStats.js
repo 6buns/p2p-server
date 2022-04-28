381574c28a9541fb3ca2f7925cdb3d6945a9bbdf
adtjha <adtjha@hotmail.com>
2022-04-28 15:24:23 +0530

    asd

--- src/redis/saveStats.js
+++ src/redis/saveStats.js
@@ -4,6 +4,7 @@ const { client } = require('.');
 exports.saveStats = (roomId, name, stats) => {
     return new Promise((resolve, reject) => {
         const roomKeyHash = createHash('md5').update(`${roomId}`).digest('hex');
+        let roomData = {};
         try {
             roomData = JSON.parse(await client.get(roomKeyHash));
             roomData.stats[name] = stats;
