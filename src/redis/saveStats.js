00ba7b9cf9c8687d486fb6f71ca00443615fe1e6
adtjha <adtjha@hotmail.com>
2022-04-28 15:40:51 +0530

    hey

--- src/redis/saveStats.js
+++ src/redis/saveStats.js
@@ -1,13 +1,20 @@
 const { createHash } = require('crypto');
 const { client } = require('.');
 
-exports.saveStats = (roomId, name, stats) => {
+exports.saveStats = async (roomId, name, stats) => {
     return new Promise((resolve, reject) => {
         const roomKeyHash = createHash('md5').update(`${roomId}`).digest('hex');
         let roomData = {};
         try {
             roomData = JSON.parse(await client.get(roomKeyHash));
-            roomData.stats[name] = stats;
+            if (roomData.hasOwnProperty(stats)) {
+                roomData[stats][name] = {};
+                roomData[stats][name] = stats;
+            } else {
+                roomData[stats] = {}
+                roomData[stats][name] = {};
+                roomData[stats][name] = stats;
+            }
             await client.set(roomKeyHash, { ...roomData })
             resolve();
         } catch (error) {
