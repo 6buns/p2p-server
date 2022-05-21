071cf9b51ea35c326dbb33d72b91da5fb2c48f49
adtjha <adtjha@hotmail.com>
2022-05-21 06:59:39 +0530

    mmhmm

--- src/redis/updateRoom.js
+++ src/redis/updateRoom.js
@@ -4,11 +4,15 @@ const { client } = require('.');
 exports.updateRoom = (roomData) => {
     return new Promise(async (resolve, reject) => {
         const roomKeyHash = createHash('md5').update(`${roomData.id}`).digest('hex');
+        let result = "";
         try {
             console.log(`UPDATE ROOM : ${roomData.id}`, { ...roomData });
-            resolve(await client.set(roomKeyHash, { ...roomData }));
+            result = await client.set(roomKeyHash, { ...roomData }, {
+                XX: true
+            });
         } catch (error) {
             reject(error.message);
         }
+        resolve(result);
     });
 };
